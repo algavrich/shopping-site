f03e4b0cc4971b31851e9dd9d2ae4f5679821386
Lucy Gavrich <annalgav@gmail.com>
2022-08-25 12:45:12 -0700

    Fixed login page, created Customer class

--- customers.py
+++ customers.py
@@ -4,4 +4,31 @@
 class Customer:
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__(self, first_name, last_name, email, password):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email 
+        self.password = password
+
+    def __repr__(self):
+
+        return (
+            f"<Customer: {self.first_name} {self.last_name}, 
+            {self.email}, 
+            {self.password}>"
+        )
+
+
+def get_customers_from_file(filepath):
+
+    customers = {}
+
+    for line in open(filepath):
+
+        line = line.split("|")
+        first_name, last_name, email, password = line 
+        customers[email] = Customer(first_name, last_name, email, password)
+
+    return customers
+
